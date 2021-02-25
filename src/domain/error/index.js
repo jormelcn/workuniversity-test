498f408e415dbd919ef37bac3675c8042922b617
Jorge Narváez <jormelcn@gmail.com>
2021-02-25 18:49:09 -0500

    feat: se definió el error InsufficientWorkingHours

--- src/domain/error/index.js
+++ src/domain/error/index.js
@@ -5,8 +5,11 @@ class NotImplementedError extends Error { }
 
 class InvalidOrderQuantity extends Error {}
 
+class InsufficientWorkingHours extends Error {}
+
 module.exports = {
     InvalidArgumentError,
     NotImplementedError,
     InvalidOrderQuantity,
+    InsufficientWorkingHours,
 }
