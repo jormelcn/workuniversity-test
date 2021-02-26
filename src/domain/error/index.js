da13c1770d177d1f8b3420761eb5025cf746c9f9
Jorge Narváez <jormelcn@gmail.com>
2021-02-26 10:34:40 -0500

    feat: se definieron los errores InaccessibleRepository y NotFoundError.

--- src/domain/error/index.js
+++ src/domain/error/index.js
@@ -7,9 +7,15 @@ class InvalidOrderQuantity extends Error {}
 
 class InsufficientWorkingHours extends Error {}
 
+class InaccessibleRepository extends Error {}
+
+class NotFoundError extends Error {}
+
 module.exports = {
     InvalidArgumentError,
     NotImplementedError,
     InvalidOrderQuantity,
     InsufficientWorkingHours,
+    InaccessibleRepository,
+    NotFoundError,
 }
