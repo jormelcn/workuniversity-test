aaf8fb32f4e86397257aa4a85c52c02802e2c731
Jorge Narváez <jormelcn@gmail.com>
2021-02-26 11:53:33 -0500

    fix: remover propiedades  vehiclesTypesList y quantityList de VehicleOrder.

--- src/domain/entity/vehicle-order.js
+++ src/domain/entity/vehicle-order.js
@@ -25,29 +25,29 @@ class VehicleOrder {
     constructor(
         id,
         orderDate,
-        vehiclesTypesList,
-        quantityList,
+        // vehiclesTypesList,
+        // quantityList,
     ){
         if(!verifyIsString(id))
             throw new InvalidArgumentError("VehicleOrder: id debe ser string");
         if(!verifyIsInstance(orderDate, Date))
             throw new InvalidArgumentError("VehicleOrder: orderDate debe ser instancia de Date");
-        if(!verifyArrayInstanceOf(vehiclesTypesList, VehicleType))
-            throw new InvalidArgumentError("VehicleOrder: vehiclesTypesList debe ser una lista de VehicleType");
-        if(!verifyArray(quantityList, verifyIsInteger))
-            throw new InvalidArgumentError("VehicleOrder: quantityList debe ser una lista de Numeros enteros");
+        // if(!verifyArrayInstanceOf(vehiclesTypesList, VehicleType))
+        //     throw new InvalidArgumentError("VehicleOrder: vehiclesTypesList debe ser una lista de VehicleType");
+        // if(!verifyArray(quantityList, verifyIsInteger))
+        //     throw new InvalidArgumentError("VehicleOrder: quantityList debe ser una lista de Numeros enteros");
 
-        if(vehiclesTypesList.length !== quantityList.length)
-            throw new InvalidArgumentError("VehicleOrder: las listas de vehiculos y cantidades deben ser de igual tamaño");
+        // if(vehiclesTypesList.length !== quantityList.length)
+        //     throw new InvalidArgumentError("VehicleOrder: las listas de vehiculos y cantidades deben ser de igual tamaño");
 
-        const totalQuantity = sumarArray(quantityList);
-        if(totalQuantity < minQuantity || totalQuantity > maxQuantity)
-            throw new InvalidOrderQuantity("VehicleOrder: La cantidad requerida excede los limites establecidos");
+        // const totalQuantity = sumarArray(quantityList);
+        // if(totalQuantity < minQuantity || totalQuantity > maxQuantity)
+        //     throw new InvalidOrderQuantity("VehicleOrder: La cantidad requerida excede los limites establecidos");
 
         this.id = is;
         this.orderDate = orderDate;
-        this.vehiclesTypesList = vehiclesTypesList;
-        this.quantityList = quantityList;
+        // this.vehiclesTypesList = vehiclesTypesList;
+        // this.quantityList = quantityList;
     }
 }
 
